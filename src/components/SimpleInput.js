c60f083659c3daba2585b54bbab758aeb46069f3
Nikhil <nikhilsindiri3@gmail.com>
2023-01-23 08:43:15 +0530

    adding right invalidations to just one input

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const SimpleInput = (props) => {
+
+  const [enteredName,setEnteredName] = useState('');
+  const [enteredNameTouched,setEnteredNameTouched] = useState(false) 
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  const nameInputIsInValid =!enteredNameIsValid && enteredNameTouched;
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+  
+  const nameInputBlurHandler=(event)=>{
+    setEnteredNameTouched(true);
+  }
+
+  const formSubmissionHandler =(event) => {
+    event.preventDefault();
+    setEnteredNameTouched(true)
+    if(!enteredNameIsValid) {
+      return;
+    }
+    console.log(enteredName)
+    
+  }
+  
+  const nameInputClasses =nameInputIsInValid ? 'form-control invalid' : 'form-control'
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' onChange={nameInputChangeHandler} value={enteredName} onBlur={nameInputBlurHandler} />
+        {nameInputIsInValid && <p className="error-text">Shouldn't be empty</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
